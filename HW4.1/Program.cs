30efb48d0202f09c3f32098db8c13b1adc64c3e4
Victor <uub.user.1@gmail.com>
2025-09-06 17:15:18 +0300

    Add project files.

---
+++ HW4.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    { 
+        Console.OutputEncoding = Encoding.UTF8;
+
+        const int minNumber = 0;
+        const int maxNumber = 10;
+
+        Console.Write($"Введіть номер дня тижня: ");
+
+        int userDayNumberInput = Convert.ToInt32(Console.ReadLine());
+
+        switch (userDayNumberInput)
+        {
+            case 1:
+                Console.WriteLine("Понеділок");
+                break;
+            case 2:
+                Console.WriteLine("Вівторок");
+                break;
+            case 3:
+                Console.WriteLine("Середа");
+                break;
+            case 4:
+                Console.WriteLine("Четвер");
+                break;
+            case 5:
+                Console.WriteLine("П'ятниця");
+                break;
+            case 6:
+                Console.WriteLine("Субота");
+                break;
+            case 7:
+                Console.WriteLine("Неділя");
+                break;
+            default:
+                Console.WriteLine($"Введене число не відповідає дню тижня. Введіть число від {minNumber} до {maxNumber}.");
+                break;
+        }
+    }
+}
